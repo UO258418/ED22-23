c2cd3bb76ba17f38c764cec4bc4ad2c811f347d9
Jonás Martínez Cuesta <UO258418@uniovi.es>
2022-09-13 19:48:53 +0200

    up to fibonacci

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String... args) {
 		System.out.println(factorial(4));
 		System.out.println(recursiveFactorial(4));
+		System.out.println(isOdd(10));
+		System.out.println(intDivision(10, 3));
+		System.out.println(reminder(10, 3));
+		System.out.println("Fibonacci: " + fibonacci(6));
+		System.out.println("RowTwo: " + powTwo(0));
+		System.out.println("RecursiveRowTwo: " + recursivePowTwo(0));
 	}
 
 	public static int factorial(int n) {
@@ -20,4 +26,52 @@ public class Main {
 		else return n * recursiveFactorial(n - 1);
 	}
 	
+	
+	public static boolean isOdd(int n) {
+		if(n == 0) return false;
+		else if(n == 1) return true;
+		else return isOdd(n - 2);
+	}
+	
+	public static int intDivision(int D, int d) {
+		if(D < d) return 0;
+		else return 1 + intDivision(D - d, d);
+	}
+	
+	public static int reminder(int D, int d) {
+		if(D < d) return D;
+		else return reminder(D - d, d);
+	}
+	
+	public static int fibonacci(int index) {
+		if(index == 1) return 0;
+		else if(index == 2) return 1;
+		else {
+			return fibonacci(index - 2) + fibonacci(index - 1);
+		}
+	}
+	
+	// TODO
+	public static boolean isSymmetric(int[][] matrix) {
+		return true;
+	}
+	
+	
+	public static int powTwo(int exp) {
+		int result = 1;
+		for(int i = 0; i < exp; i++) 
+			result *= 2;
+		return result;
+	}
+	
+	public static int recursivePowTwo(int exp) {
+		return (exp == 1 || exp == 0) ? 1 : 2 * powTwo(exp - 1);
+	}
+	
 }
+
+
+
+
+
+
--- test/main/TestMain.java
+++ test/main/TestMain.java
@@ -1,12 +1,15 @@
 package main;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class TestMain {
 
 	@Test
-	void test() {
+	void testFactorial() {
 		assertEquals(1, Main.recursiveFactorial(0));
 		assertEquals(1, Main.recursiveFactorial(1));
 		assertEquals(2, Main.recursiveFactorial(2));
@@ -18,6 +21,51 @@ class TestMain {
 		assertEquals(40320, Main.recursiveFactorial(8));
 		assertEquals(362880, Main.recursiveFactorial(9));
 		assertEquals(3628800, Main.recursiveFactorial(10));
+	}
+	
+	@Test
+	void testOdd() {
+		assertFalse(Main.isOdd(0));
+		assertTrue(Main.isOdd(1));
+		assertFalse(Main.isOdd(2));
+		assertTrue(Main.isOdd(3));
+		assertFalse(Main.isOdd(4));
+		assertTrue(Main.isOdd(5));
+		assertFalse(Main.isOdd(6));
+		assertTrue(Main.isOdd(7));
+		assertFalse(Main.isOdd(8));
+		assertTrue(Main.isOdd(9));
+		assertFalse(Main.isOdd(10));
+	}
+	
+	@Test
+	void testFibonacci() {
+		assertEquals(0, Main.fibonacci(1));
+		assertEquals(1, Main.fibonacci(2));
+		assertEquals(1, Main.fibonacci(3));
+		assertEquals(2, Main.fibonacci(4));
+		assertEquals(3, Main.fibonacci(5));
+		assertEquals(5, Main.fibonacci(6));
+	}
+	
+	@Test
+	void testPowTwo() {
+		assertEquals(1, Main.powTwo(0));
+		assertEquals(8, Main.powTwo(3));
+		assertEquals(16, Main.powTwo(4));
+		assertEquals(32, Main.powTwo(5));
+		assertEquals(64, Main.powTwo(6));
+		assertEquals(128, Main.powTwo(7));
+	}
+	
+	@Test
+	void testRecursivePowTwo() {
+		assertEquals(1, Main.recursivePowTwo(0));
+		assertEquals(8, Main.recursivePowTwo(3));
+		assertEquals(16, Main.recursivePowTwo(4));
+		assertEquals(32, Main.recursivePowTwo(5));
+		assertEquals(64, Main.recursivePowTwo(6));
+		assertEquals(128, Main.recursivePowTwo(7));
 	}
 
 }
